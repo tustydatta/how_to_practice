e38266b09042331e36a306cfd84f35d9ceec9e0c
tustydatta <88793752+tustydatta@users.noreply.github.com>
2021-08-13 09:10:01 +0600

    Delete cookies_1.php

--- cookies_1.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!isset($_COOKIE['user'])) {
-     echo "Cookie name is not set!";
-} else {
-     echo "Cookie " .'user' . "is set! <br>";
-     echo "Value is: " . $_COOKIE['user'];
-}
-
-?>
